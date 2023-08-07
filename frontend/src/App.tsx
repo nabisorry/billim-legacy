73d27e3971e649fe7d36dc839b44f64982b34729
nabisorry <cdb131450@gmail.com>
2023-08-08 06:44:39 +0900

    <fix> app 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,24 +4,13 @@ import { useNavigate } from "react-router";
 
 // 내부모듈
 import DesktopPage from "@pages/common/DesktopPage";
+import HomePage from "@pages/HomePage";
 
 function App() {
   const navigator = useNavigate();
   const isMobile = useMediaQuery("(max-width:600px)");
 
-  return (
-    <div>
-      {isMobile ? (
-        <>
-          <button onClick={() => navigator("/login")}>로그인페이지</button>
-          <button onClick={() => navigator("/register")}>회원가입페이지</button>
-          <button onClick={() => navigator("/home")}>홈페이지</button>
-        </>
-      ) : (
-        <DesktopPage />
-      )}
-    </div>
-  );
+  return <div>{isMobile ? <HomePage /> : <DesktopPage />}</div>;
 }
 
 export default App;
